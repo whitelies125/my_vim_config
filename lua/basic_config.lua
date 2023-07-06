b4365a54e79cbd1fcf8623e65c75b7dad60054cc
whitelies125 <lzb125125@gmail.com>
2023-07-06 23:50:24 +0800

    migrate from Vim to Neovim, from Vim script to Lua.

---
+++ lua/basic_config.lua
@@ -0,0 +1,40 @@
+print('require basic_config begin')
+
+-- 一些与 nvim 本身相关的、比较基础的选项设置
+
+-- vim 中的选项，在 lua 中是 vim.o(类型为 table) 中的变量
+
+-- 对 "" 无名寄存器进行操作时，也对 "+ 寄存器进行同样的操作(unnamed 是对 "*)
+vim.o.clipboard = 'unnamedplus'
+-- 设置不自动折行
+vim.o.wrap = false
+
+-- 设置当前行显示为绝对行号，其它行显示为相对行号
+vim.o.number = true
+vim.o.relativenumber = true
+-- 设置显示光标坐标（的行号,列号），如果还有空间，在最右端显示文本在该文件中的相对位置
+vim.o.ruler = true
+-- 设置光标所在当前行显示为高亮
+vim.o.cursorline = true
+-- 设置光标所在当前列显示为高亮
+-- vim.o.cursorcolumn = true
+-- 设置第 81 列高亮
+vim.o.colorcolumn = '81'
+
+-- 设置搜索时忽略大小写进行匹配
+vim.o.ignorecase = true
+-- 设置如果搜索的字符串中包含大写字母，则不适用 ignorecase，即不再忽略大小写
+-- 该设置仅在开启 ignorecase 选项，且正在输入搜索字符串时且才生效
+vim.o.smartcase = true
+
+-- 设置显示 tab 键时对应的 space 数，注意仅仅只是修改了 tab 键显示长度，而并非是将 tab 键转换为 4 个 space
+vim.o.tabstop = 4
+-- 设置在插入模式中，插入 tab 时将其转换为合适数量的 space，这个选项才是转换 tab 为 space
+vim.o.expandtab = true
+-- 设置(自动)缩进使用的空白数目，例如作用于使用 <<，>> 命令时。shiftwidth=0 表示使用与 tabstop 相同的数值
+vim.o.shiftwidth = 0
+
+-- 设置补全列表选项
+vim.o.completeopt = 'menuone,preview,noinsert'
+
+print('require basic_config end')
---
+++ lua/keymaps.lua
@@ -0,0 +1,37 @@
+print('require keymaps begin')
+
+-- 按键映射相关配置
+
+-- 修改 <Leader> 键为空格键，默认值为反斜杠 \
+vim.g.mapleader = " "
+
+
+local opt_default = { noremap = true, silent = true }
+local mode_i = { 'i' }
+local mode_ni = { 'n', 'i' }
+
+-- 插入模式映射
+-- inoremap int8 int8_t
+-- vim.keymap.set('i', 'int8', 'int8_t', {noremap = true, silent = true})
+--
+local keymaps = {
+    { mode = mode_i, from = 'i8', to = 'int8_t',  opt = opt_default },
+    { mode = mode_i, from = 'i16', to = 'int16_t',  opt = opt_default },
+    { mode = mode_i, from = 'i32', to = 'int32_t',  opt = opt_default },
+    { mode = mode_i, from = 'i8m', to = 'INT8_MAX',  opt = opt_default },
+    { mode = mode_i, from = 'i16m', to = 'INT16_MAX',  opt = opt_default },
+    { mode = mode_i, from = 'i32m', to = 'INT32_MAX',  opt = opt_default },
+
+    { mode = mode_i, from = 'u8', to = 'uint8_t',  opt = opt_default },
+    { mode = mode_i, from = 'u16', to = 'uint16_t',  opt = opt_default },
+    { mode = mode_i, from = 'u32', to = 'uint32_t',  opt = opt_default },
+    { mode = mode_i, from = 'u8m', to = 'UINT8_MAX',  opt = opt_default },
+    { mode = mode_i, from = 'u16m', to = 'UINT16_MAx',  opt = opt_default },
+    { mode = mode_i, from = 'u32m', to = 'UINT32_MAx',  opt = opt_default },
+}
+
+for _, v in ipairs(keymaps) do
+    vim.keymap.set(v.mode, v.from, v.to, v.opt)
+end
+
+print('require keymaps end')
