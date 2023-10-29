0c79140948bad6bfcd882a85fa585f479b2402bb
whitelies125 <lzb125125@gmail.com>
2023-10-29 22:31:58 +0800

    add lua-language-server.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,7 @@ return {
     -- 开启懒加载, 在触发特定事件 or 执行特定命令 or 打开特定类型文件 or 输入特定按键时才加载该插件
     lazy = true,
     -- lazy == true 时，当打开特定类型文件时，才加载该插件
-    ft = { "c", "cpp", "h", },
+    ft = { "c", "cpp", "h", "lua" },
     -- 当 lazy.nvim 加载该插件时，将会执行 config.
     -- config 可设置为
     -- true：表示执行 lazy.nvim 提供的默认实现的函数，该默认实现会执行该插件的 setup(opts) 函数
@@ -14,8 +14,16 @@ return {
         -- Setup language servers.
         local lspconfig = require('lspconfig')
         -- 启动 clangd
-        lspconfig.clangd.setup {}
-
+        local c_cpp_ft = {'c', 'cpp', 'h'}
+        for i = 1, #c_cpp_ft do
+            if c_cpp_ft[i] == vim.bo.filetype then
+                lspconfig.clangd.setup {}
+                break
+            end
+        end
+        if vim.bo.filetype == 'lua' then
+            lspconfig.lua_ls.setup {}
+        end
         -- Global mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
         vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
