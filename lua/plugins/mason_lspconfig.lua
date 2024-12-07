bf8f1f1ae05e63ad5c1f9b8da72827b31b17947c
whitelies125 <lzb125125@gmail.com>
2024-12-08 07:13:15 +0800

    change lsp setup config.

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -5,10 +5,6 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
     },
-    -- 开启懒加载, 在触发特定事件 or 执行特定命令 or 打开特定类型文件 or 输入特定按键时才加载该插件
-    lazy = true,
-    -- lazy == true 时，当打开特定类型文件时，才加载该插件
-    ft = { "c", "cpp", "h", "lua" },
     -- 当 lazy.nvim 加载该插件时，将会执行 config.
     -- config 可设置为
     -- true：表示执行 lazy.nvim 提供的默认实现的函数，该默认实现会执行该插件的 setup(opts) 函数
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,9 +8,9 @@ return {
         'hrsh7th/cmp-nvim-lsp',
     },
     -- 开启懒加载, 在触发特定事件 or 执行特定命令 or 打开特定类型文件 or 输入特定按键时才加载该插件
-    lazy = true,
+    -- lazy = true,
     -- lazy == true 时，当打开特定类型文件时，才加载该插件
-    ft = { "c", "cpp", "h", "lua", "python" },
+    -- ft = { "c", "cpp", "h", "lua", "python" },
     -- 当 lazy.nvim 加载该插件时，将会执行 config.
     -- config 可设置为
     -- true：表示执行 lazy.nvim 提供的默认实现的函数，该默认实现会执行该插件的 setup(opts) 函数
@@ -20,30 +20,20 @@ return {
         local lspconfig = require('lspconfig')
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
         -- 启动 clangd
-        local c_cpp_ft = { 'c', 'cpp', 'h' }
-        for i = 1, #c_cpp_ft do
-            if c_cpp_ft[i] == vim.bo.filetype then
-                lspconfig.clangd.setup {
-                    capabilities = capabilities
-                }
-                break
-            end
-        end
-        if vim.bo.filetype == 'lua' then
-            lspconfig.lua_ls.setup {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" }
-                        },
-                        capabilities = capabilities
+        lspconfig.clangd.setup {
+            capabilities = capabilities
+        }
+        lspconfig.lua_ls.setup {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" }
                     },
+                    capabilities = capabilities
                 },
-            }
-        end
-        if vim.bo.filetype == 'python' then
-            lspconfig.pyright.setup {}
-        end
+            },
+        }
+        lspconfig.pyright.setup {}
         -- Global mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
         local map_opts = function(str)
