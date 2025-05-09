141f073b9d8892189b31a6a9492da1a5a202cc8e
whitelies125 <lzb125125@gmail.com>
2025-05-10 03:58:26 +0800

    update plugins; updata nvim to v0.11.1

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -13,49 +13,47 @@ return {
     -- true：表示执行 lazy.nvim 提供的默认实现的函数，该默认实现会执行该插件的 setup(opts) 函数
     -- fun(LazyPlugin, opts:table): 执行 config 定义的回调函数
     config = function(_, opts)
-        local lspconfig = require('lspconfig')
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        vim.lsp.config('*', {
+            capabilities = capabilities,
+        });
+        vim.lsp.config('lua_ls', {
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = 'LuaJIT',
+                    },
+                    diagnostics = {
+                        globals = {
+                            'vim',
+                        },
+                    },
+                },
+            },
+        })
         require("mason-lspconfig").setup {
             -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
-            -- This setting has no relation with the `automatic_installation` setting.
             ---@type string[]
             ensure_installed = { "lua_ls", "clangd", "pyright" },
 
-            -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
-            -- This setting has no relation with the `ensure_installed` setting.
-            -- Can either be:
-            --   - false: Servers are not automatically installed.
-            --   - true: All servers set up via lspconfig are automatically installed.
-            --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
-            --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
-            ---@type boolean
-            automatic_installation = false,
-
-            -- See `:h mason-lspconfig.setup_handlers()`
-            ---@type table<string, fun(server_name: string)>?
-            handlers = {
-                -- The first entry (without a key) will be the default handler
-                -- and will be called for each installed server that doesn't have
-                -- a dedicated handler.
-                function(server_name) -- default handler (optional)
-                    lspconfig[server_name].setup {
-                        capabilities = capabilities,
-                    }
-                end,
-                -- Next, you can provide a dedicated handler for specific servers.
-                -- For example, a handler override for the `rust_analyzer`:
-                ['lua_ls'] = function()
-                    lspconfig.lua_ls.setup {
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" }
-                                },
-                            },
-                        },
-                    }
-                end
-            }
+            -- Whether installed servers should automatically be enabled via `:h vim.lsp.enable()`.
+            --
+            -- To exclude certain servers from being automatically enabled:
+            -- ```lua
+            --   automatic_enable = {
+            --     exclude = { "rust_analyzer", "ts_ls" }
+            --   }
+            -- ```
+            --
+            -- To only enable certain servers to be automatically enabled:
+            -- ```lua
+            --   automatic_enable = {
+            --     "lua_ls",
+            --     "vimls"
+            --   }
+            -- ```
+            ---@type boolean | string[] | { exclude: string[] }
+            automatic_enable = true,
         }
     end,
 }
