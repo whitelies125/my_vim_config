823b5dcbc3f21cf5e317a3fa72d0883122d4bee1
whitelies125 <lzb125125@gmail.com>
2026-02-11 15:22:49 +0800

    trouble.nvim add large result filter.

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,36 +5,53 @@ return
         auto_preview = false,         -- 指定不自动预览
         focus = true,                 -- 指定窗口出现时自动聚焦
         win = { position = "right" }, -- 指定窗口出现在右侧
+        filter = function(items)
+            if (#items > 200) then    -- trouble.nvim default show max_items is 200
+                print("Trouble: Over Size! total result num : " .. #items .. ". every filename only show 1 result.")
+                local set = {}
+                local reduce_items = {}
+                for _, it in ipairs(items) do
+                    print(unpack(it))
+                    if not set[it.filename] then
+                        table.insert(reduce_items, it)
+                        set[it.filename] = true;
+                    end
+                end
+                return reduce_items;
+            else
+                return items
+            end
+        end,
     },
     cmd = "Trouble",
     keys = {
         {
-            "<leader>xx",
+            "<leader>td",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
         },
         {
-            "<leader>xX",
+            "<leader>tD",
             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
             desc = "Buffer Diagnostics (Trouble)",
         },
         {
-            "<leader>cs",
+            "<leader>ts",
             "<cmd>Trouble symbols toggle focus=false<cr>",
             desc = "Symbols (Trouble)",
         },
         {
-            "<leader>cl",
+            "<leader>tl",
             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
             desc = "LSP Definitions / references / ... (Trouble)",
         },
         {
-            "<leader>xL",
+            "<leader>tl",
             "<cmd>Trouble loclist toggle<cr>",
             desc = "Location List (Trouble)",
         },
         {
-            "<leader>xQ",
+            "<leader>tq",
             "<cmd>Trouble qflist toggle<cr>",
             desc = "Quickfix List (Trouble)",
         },
